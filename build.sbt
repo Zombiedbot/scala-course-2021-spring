74d3864546cabe0049dc040965810ff08aadb7a9
Igor Wolkov <igor.wolkov@scala.group>
2021-02-25 22:31:47 +0200

    Week 1: topic and homework

---
+++ build.sbt
@@ -0,0 +1,30 @@
+import Dependencies._
+
+lazy val root = project
+  .in(file("."))
+  .settings(name := "scala-course-2021-spring")
+  .settings(commonSettings: _*)
+  .settings(libraryDependencies ++= dependencies)
+  .settings(testFrameworks += new TestFramework("munit.Framework"))
+  .settings(Test / parallelExecution := false)
+
+lazy val dependencies =
+    cats ++
+    `cats-effect`
+
+testFrameworks += new TestFramework("munit.Framework")
+
+lazy val commonSettings = Seq(
+    scalaVersion := "3.0.0-M3",
+    organization := "karazin-scala-users-group",
+    version      := "0.1.0-SNAPSHOT",
+    scalacOptions ++= Seq(
+        "-feature",
+        "-deprecation",
+        "-Xfatal-warnings",
+    ),
+    javacOptions ++= Seq(
+        "-source", "11",
+        "-target", "11"
+    ),
+)
---
+++ project/Dependencies.scala
@@ -0,0 +1,27 @@
+import sbt._
+
+object Dependencies {
+  
+  object Version {
+    val `scala3-compiler`             = "3.0.0-M3"
+    val cats                          = "2.3.1"
+    val `cats-effect`                 = "3.0.0-M5"
+  }
+
+  lazy val `scala3-compiler`: Seq[ModuleID] = Seq(
+    "org.scala-lang" % "scala3-compiler_3.0.0-M3",
+  ).map(_ % Version.`scala3-compiler` withSources() withJavadoc())
+
+  lazy val cats: Seq[ModuleID] = Seq(
+    "org.typelevel" %% "cats-core",
+    "org.typelevel" %% "cats-laws",
+    "org.typelevel" %% "cats-free",
+    "org.typelevel" %% "cats-testkit",
+    "org.typelevel" %% "alleycats-core"
+  ).map(_ % Version.cats withSources() withJavadoc())
+
+  lazy val `cats-effect`: Seq[ModuleID] = Seq(
+    "org.typelevel" %% "cats-effect"
+  ).map(_ % Version.`cats-effect` withSources() withJavadoc())
+
+}
