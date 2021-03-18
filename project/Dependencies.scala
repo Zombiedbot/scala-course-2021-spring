77810c40d0ce4cf51bc65a579d069d6268cf5a7c
user <artem.yena@cruxlab.com>
2021-03-18 22:38:09 +0200

    Updated build config

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,7 @@ object Dependencies {
     val `scala3-compiler`             = "3.0.0-M3"
     val cats                          = "2.3.1"
     val `cats-effect`                 = "3.0.0-M5"
+    val munit                         = "0.7.22"
   }
 
   lazy val `scala3-compiler`: Seq[ModuleID] = Seq(
@@ -23,5 +24,9 @@ object Dependencies {
   lazy val `cats-effect`: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-effect"
   ).map(_ % Version.`cats-effect` withSources() withJavadoc())
+  
+  lazy val munit: Seq[ModuleID] = Seq(
+    "org.scalameta" %% "munit"
+  ).map(_ % Version.munit % Test withSources() withJavadoc())  
 
 }
