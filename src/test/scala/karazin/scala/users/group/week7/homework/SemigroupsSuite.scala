4a563f233710960658ce38dae687e0c5bb43545a
user <artem.yena@cruxlab.com>
2021-04-26 22:47:19 +0300

    Fixed according to tasks

--- src/test/scala/karazin/scala/users/group/week7/homework/SemigroupsSuite.scala
+++ src/test/scala/karazin/scala/users/group/week7/homework/SemigroupsSuite.scala
@@ -23,33 +23,44 @@ import karazin.scala.users.group.week7.homework.semigroups.{given, _}
 import scala.language.postfixOps
 
 object SemigroupsSuite extends Properties("Semigroup Basic types"):
+  
+  def testCombine[T](first: Map[String, T], second: Map[String, T])(using Semigroup[T]): Map[String, T] = 
+    var res = collection.mutable.Map[String, T]()
+    res = (res ++ first)
+    second foreach { (key, value) =>
+      res.get(key) match 
+        case Some(anotherValue) => res.update(key, anotherValue combine value)
+        case _                  => res.update(key, value)
+    }
+    res.toMap
+    
 
   property("semigroup Map[String, Int]") =
     forAll { (left: Map[String, Int], right: Map[String, Int]) =>
-      (left combine right) == (left ++ right)
+      (left combine right) == testCombine[Int](left, right)
     }
 
   property("semigroup Map[String, String]") =
     forAll { (left: Map[String, String], right: Map[String, String]) =>
-      (left combine right) == (left ++ right)
+      (left combine right) == testCombine[String](left, right)
     }
   
   property("semigroup Map[String, Boolean]") =
     forAll { (left: Map[String, Boolean], right: Map[String, Boolean]) =>
-      (left combine right) == (left ++ right)
+      (left combine right) == testCombine[Boolean](left, right)
     }
   
   property("semigroup Map[String, List[Int]]") =
     forAll { (left: Map[String, List[Int]], right: Map[String, List[Int]]) =>
-      (left combine right) == (left ++ right)
+      (left combine right) == testCombine[List[Int]](left, right)
     }
   
   property("semigroup Map[String, List[String]]") =
     forAll { (left: Map[String, List[String]], right: Map[String, List[String]]) =>
-      (left combine right) == (left ++ right)
+      (left combine right) == testCombine[List[String]](left, right)
     }
   
   property("semigroup Map[String, List[Boolean]]") =
     forAll { (left: Map[String, List[Boolean]], right: Map[String, List[Boolean]]) =>
-      (left combine right) == (left ++ right)
+      (left combine right) == testCombine[List[Boolean]](left, right)
     }
