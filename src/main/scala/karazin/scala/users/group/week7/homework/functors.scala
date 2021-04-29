5c6b794c87bc069be016751964b07cab35bb6fe8
user <artem.yena@cruxlab.com>
2021-04-29 22:13:42 +0300

    Added custom type

--- src/main/scala/karazin/scala/users/group/week7/homework/functors.scala
+++ src/main/scala/karazin/scala/users/group/week7/homework/functors.scala
@@ -1,12 +1,14 @@
 package karazin.scala.users.group.week7.homework
 
 object functors:
+  
+  type MapStr[T] = Map[String, T]
 
-  trait Functor[F[String, _]]:
-    def map[A, B](x: F[String, A])(f: A => B): F[String, B]
+  trait Functor[F[_]]:
+    def map[A, B](x: F[A])(f: A => B): F[B]
     
-  given MapFunctor[T]: Functor[Map] with
-    def map[A, B](x: Map[String, A])(f: A => B): Map[String, B] = x.view.mapValues(f).toMap
+  given MapFunctor[T]: Functor[MapStr] with
+    def map[A, B](x: MapStr[A])(f: A => B): MapStr[B] = x.view.mapValues(f).toMap
 
   /*
    Implement Functor[Map[String, T]] for any type T
--- src/test/scala/karazin/scala/users/group/week7/homework/FunctorsSuite.scala
+++ src/test/scala/karazin/scala/users/group/week7/homework/FunctorsSuite.scala
@@ -27,47 +27,47 @@ object FunctorsSuiteBasic extends Properties("Functor Basic types"):
 
   property("functor Map[String, Int] to int") =
     forAll { (map: Map[String, Int], f: Int => Int) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap    
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap    
     }
 
   property("functor Map[String, Int] to string") =
     forAll { (map: Map[String, Int], f: Int => String) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, Int] to boolean") =
     forAll { (map: Map[String, Int], f: Int => Boolean) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, String] to int") =
     forAll { (map: Map[String, String], f: String => Int) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, String] to string") =
     forAll { (map: Map[String, String], f: String => String) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, String] to boolean") =
     forAll { (map: Map[String, String], f: String => Boolean) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, Boolean] to int") =
     forAll { (map: Map[String, Boolean], f: Boolean => Int) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, Boolean] to string") =
     forAll { (map: Map[String, Boolean], f: Boolean => String) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, Boolean] to boolean") =
     forAll { (map: Map[String, Boolean], f: Boolean => Boolean) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
 
@@ -75,45 +75,45 @@ object FunctorsSuiteLists extends Properties("Functor Basic types"):
 
   property("functor Map[String, List[Int]] to List[Int]") =
     forAll { (map: Map[String, List[Int]], f: List[Int] => List[Int]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[Int]] to List[String]") =
     forAll { (map: Map[String, List[Int]], f: List[Int] => List[String]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[Int]] to List[Boolean]") =
     forAll { (map: Map[String, List[Int]], f: List[Int] => List[Boolean]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[String]] to List[Int]") =
     forAll { (map: Map[String, List[String]], f: List[String] => List[Int]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[String]] to List[String]") =
     forAll { (map: Map[String, List[String]], f: List[String] => List[String]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[String]] to List[Boolean]") =
     forAll { (map: Map[String, List[String]], f: List[String] => List[Boolean]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[Boolean]] to List[Int]") =
     forAll { (map: Map[String, List[Boolean]], f: List[Boolean] => List[Int]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[Boolean]] to List[String]") =
     forAll { (map: Map[String, List[Boolean]], f: List[Boolean] => List[String]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
 
   property("functor Map[String, List[Boolean]] to List[Boolean]") =
     forAll { (map: Map[String, List[Boolean]], f: List[Boolean] => List[Boolean]) =>
-      summon[Functor[Map]].map(map)(f) == map.view.mapValues(f).toMap
+      summon[Functor[MapStr]].map(map)(f) == map.view.mapValues(f).toMap
     }
