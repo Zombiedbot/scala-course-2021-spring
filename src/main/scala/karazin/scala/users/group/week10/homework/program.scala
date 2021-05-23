7a0701f56d0671ee85f80232df3c69498c8c9052
user <artem.yena@cruxlab.com>
2021-05-23 20:54:01 +0300

    Implemented Homework 10

--- src/main/scala/karazin/scala/users/group/week10/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week10/homework/program.scala
@@ -1,5 +1,9 @@
 package karazin.scala.users.group.week10.homework
 
+import scala.util.{Try,Success,Failure}
+import cats.Applicative
+import cats.implicits._
+
 object program:
 
   /*
@@ -9,8 +13,18 @@ object program:
 
   object applicatives:
   
-    ???
+    val f: (Int, Double) => String = (i, d) => i.toString() + ' ' + d.toString()
     
+    val int: Try[Int] = Success(42)
+    
+    val double: Try[Double] = Success(2.28)
+    
+    val func: Try[Double => String] =
+      int.map(i => (d: Double) => f(i, d))
+    
+    val result: Try[String] =
+      Applicative[Try].ap[Double, String](func)(double)
+      
   end applicatives
 
 end program
